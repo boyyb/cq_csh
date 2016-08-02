c051df4a2fe25375d2901d977d48b807f5056b1a
yangbin <619280492@qq.com>
2016-08-02 14:56:10 +0800

    csh

---
+++ 1templet/index1.php
@@ -0,0 +1,231 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+<title>美丽长寿湖</title>
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+<link href="e/templates/default/style.css" type="text/css" rel="stylesheet" />
+<script src="e/js/jquery.min.js" type="text/javascript"></script>
+<script src="e/js/comm.js" type="text/javascript"></script><script src="e/js/zh-cn/lang.js" type="text/javascript"></script><script type="text/javascript">var submenu_style=1;</script>
+<link rel="stylesheet" type="text/css" href="e/css/dropmenu.css" />
+<script src="e/js/script.js" type="text/javascript"></script>
+<script src="e/js/QQ.js" type="text/javascript"></script>
+<script src="e/js/function.js" type="text/javascript"></script>
+</head>
+<body>
+<div class="page_style lanmupage page_l61">
+<div class="top_box">
+ <div class="top_box_inner">
+   <div class="logo"></div>
+   <div class="topcontent"><span class="top-l">欢迎光临长寿湖旅游网站!</span><span class="top-r">
+<script src="e/js/TtoS.js"></script>
+<a href="javascript:AddFavorite(location.href,'网站名称')">加入收藏</a><a href="#" onclick="setHome(this,'http://www.pageadmin.net')">设为首页</a></span> 
+<script type="text/javascript">
+function AddFavorite(sURL, sTitle)
+{
+    try
+    {
+		if( typeof(sURL) =="undefined" || sURL.length==0) { sURL=window.location; }
+		if( typeof(sTitle) =="undefined" || sTitle.length==0) { sTitle=document.title; } 
+        window.external.addFavorite(sURL, sTitle);
+    }
+    catch (e)
+    {
+        try
+        {
+            window.sidebar.addPanel(sTitle, sURL, "");
+        }
+        catch (ee)
+        {
+            alert("您使用的浏览器不支持此功能，请按“Ctrl + D”键手工加入收藏!");
+        }
+    }
+}
+function setHome(objid,urls) {
+	var a = objid, b = urls, c = "unknown"; 
+	try {
+	    if( typeof(b) =="undefined" || b.length==0) { b=location.href; }		
+		a.style.behavior = "url(#default#homepage)";
+		var d = a.isHomePage(b);
+		d = (d = d || a.isHomePage(b + "/")) || a.isHomePage(b + "/#");
+		d = "" + d;
+	   if (d != "true"){
+			a.setHomePage(b);
+	   }else{
+			alert("您已经设为首页了!");
+	   }
+	} catch (e) {
+		alert("您的浏览器不支持自动设置主页，请使用浏览器菜单手动设置!");
+	}
+};
+</script></div><div class="clear"></div>
+   <div class="menu_box" id="Menu"><ul>
+<li class="menu_style_homepage menu_current" id="MenuItem61" name="MenuItem"><a href="" class="menu">网站首页</a></li>
+<li class="menu_style" id="MenuItem62" name="MenuItem"><a href="gywm/" class="menu">景区资讯</a></li>
+<li class="menu_style" id="MenuItem67" name="MenuItem"><a href="cpzx/" class="menu">吃·住·玩·购</a>
+    <ul>
+    <li><a href="#">"吃"在长寿湖</a></li>
+    <li><a href="#">"住"在长寿湖</a></li>
+    <li><a href="#">"玩"在长寿湖</a></li>
+    <li><a href="#">"购"在长寿湖</a></li>
+    </ul>
+</li>
+<li class="menu_style" id="MenuItem65" name="MenuItem"><a href="zxly/" class="menu">在线留言</a></li>
+<li class="menu_style" id="MenuItem81" name="MenuItem"><a href="lxfx/" class="menu">联系我们</a></li>
+</ul></div>
+ </div>
+</div>
+<script type="text/javascript">var IsPageHome="1";var Lanmu_Id="61";var Sublanmu_Id="0";ShowSubMenu(Lanmu_Id);</script>
+<div class="banner"><script type="text/javascript">
+var Show_Style=3;
+var Image_12=new Array();
+var Pics="e/css/images/banner1.jpg|e/css/images/banner2.jpg";
+var Links="";
+var Titles="";
+var Alts="";
+var Apic12=Pics.split('|');
+var ALink12=Links.split('|');
+var ATitle12=Titles.split('|');
+var AAlts12=Alts.split('|');
+var Show_Text=0;
+for(i=0;i<Apic12.length;i++)
+  {
+   Image_12.src = Apic12[i]; 
+  }
+
+
+
+  var FHTML='<div id="js_slide_focus_12" class="slide_focus focus_style3" style="height:399px"><a class="prev"></a><a class="next"></a>';
+  FHTML+='<ul class="inner">';
+  for(var i=0;i<Apic12.length;i++)
+   {
+     if(ALink12.length<(i+1) || ALink12[i]=="")
+      {
+       ALink12[i]="javascript:void(0)";
+      }
+     if(AAlts12.length<(i+1))
+      {
+       AAlts12[i]="";
+      }
+     if(ATitle12.length<(i+1))
+      {
+       ATitle12[i]="";
+      }
+    FHTML+='<li><a href="'+ALink12[i]+'" target="_self" title="'+AAlts12[i]+'"><img src="'+Apic12[i]+'">';
+    FHTML+='<em>'+ATitle12[i]+'</em>';
+    FHTML+='</a></li>';
+   }
+ FHTML+='</ul>';
+ FHTML+='</div>';
+ document.write(FHTML);
+$(function(){Slide_Focus("js_slide_focus_12",1,5,1440,399,true);});
+
+</script></div>
+<div class="main_box main_box_style">
+ <div class="main_box_inner">
+ 
+
+<span class="h-cpzx-t">产品中心</span>
+<ul class="h-cpzx-fl">
+<li>
+<a href="index.aspx?lanmuid=67&sublanmuid=656"><img src="e/css/images/cpzx.jpg"/></a>
+<span>工业机械</span>
+<div>取代人工取物或置物等工作。使整个生产线更有规律性、稳定性，持续性，以保证恒久不变的生产工作状态，有效地提高生产效率、产品质量稳定性。</div>
+<a class="cpzx-cksq" href="cpzx/">查看详情</a>
+</li>
+<li>
+<a href="index.aspx?lanmuid=67&sublanmuid=657"><img src="e/css/images/cpzx1.jpg"/></a>
+<span>工业五金</span>
+<div>采用国际最先进的重力浇铸工艺，经精密机械加工制造，浑然天成，坚固耐用阀芯采用精密进口的陶瓷阀芯，拥有钻石般的硬度。</div>
+<a class="cpzx-cksq" href="cpzx/">查看详情</a>
+</li>
+<li>
+<a href="index.aspx?lanmuid=67&sublanmuid=658"><img src="e/css/images/cpzx2.jpg"/></a>
+<span>工业机床</span>
+<div>在车床上可用钻头、滚花工具等进行相应的加工。主要用于加工轴、盘、套和其他具有回转表面的工件，是制造和修配工厂中使用最广的一类机床。</div>
+<a class="cpzx-cksq" href="cpzx/">查看详情</a>
+</li>
+</ul>
+
+
+
+   
+
+
+
+<div id="module_180" style="width:500px;" class="module_box_layout_left">
+ <div class="module_box_inner">
+   <div class="module_title"><span class="module_sign">关于我们</span><span class="module_more"></span></div>
+   <div id="module_content_180" style="height:275px;" class="module_content module_content_introduct">
+
+<div style="color:#5A5A5A;font-size:14px;">
+有出口商品生产企业最高荣誉之称的“国家出口免验”资格，在我国诞生XX年来，第一次落在了工程机械企业的头上。X月X日，XX集团XX重型机械有限公 司在全国工程机械行业和XX地区首家获得出口免验资格，标志着XX重型产品质量达到国际先进水平，进入国际市场步伐加快。<br/><br/>
+
+出口免验是国家质检总局贯彻以质取胜战略，鼓励优质产品出口，促进民族品牌拓展国际市场，转变外贸增长方式，提高“中国制造”市场竞争力的重要举措， 是国家授予出口企业的最高质量荣誉，代表了行业最高水平，因此“国家出口免验”也被称为通向国际市场的“金牌通行证”。
+</div>
+<a class="gywm-gd" href="gywm/">更多...</a>
+
+   </div>
+   <div class="module_footer"><span class="l"></span><span class="r"></span></div>
+ </div>
+</div>
+
+
+
+<div id="module_181" style="width:462px;" class="module_box_layout_right">
+ <div class="module_box_inner">
+   <div id="module_content_181" style="height:320px;" class="module_content">
+<ul class="tlb">
+
+<li><a href="gywm/"><img src="e/upload/s1/article/image/2015/08/10162038.jpg"/></a></li>
+
+<li><a href="gywm/"><img src="e/upload/s1/article/image/2015/08/10162021.jpg"/></a></li>
+
+<li><a href="gywm/"><img src="e/upload/s1/article/image/2015/08/10162000.jpg"/></a></li>
+
+<li><a href="gywm/"><img src="e/upload/s1/article/image/2015/08/10161941.jpg"/></a></li>
+
+<li><a href="gywm/"><img src="e/upload/s1/article/image/2015/08/10161922.jpg"/></a></li>
+
+<li><a href="gywm/"><img src="e/upload/s1/article/image/2015/08/tpic_10161831.jpg"/></a></li>
+
+</ul></div>
+   <div class="module_footer"><span class="l"></span><span class="r"></span></div>
+ </div>
+</div>
+<div class="clear"></div>
+
+<div id="module_182" class="module_box">
+ <div class="module_box_inner">
+   <div id="module_content_182" style="height:185px;" class="module_content">
+<ul class="h-xwzx">
+
+<li class="i_1">
+<span class="title"><a href="xwzx/" target="_self" title="工业纯氧制造机中国成为第二大工业制造国" style="">工业纯氧制造机中国成为第二大工业制造国</a></span>
+<p class="introduct">xxxx年中xx制造业在全球制造业总值中所占比例已达15.6%，取代xx成为仅次于美国的全球第二大工业制造国。</p>
+</li>
+
+<li class="i_2">
+<span class="title"><a href="xwzx/" target="_self" title="公司全力开发随车吊底盘" style="">公司全力开发随车吊底盘</a></span>
+<p class="introduct">随着汽车市场专业化趋势日渐明朗，专用车在汽车销售中所占的比重越来越大。XX汽车公司全力开发随车吊底盘。</p>
+</li>
+
+<li class="i_3">
+<span class="title"><a href="xwzx/" target="_self" title="工业规划注重转方式调结构 47个行业草案已制定完成" style="">工业规划注重转方式调结构 47个行业草案已制定完成</a></span>
+<p class="introduct">十二五工业发展规划核心起草小组成员、工信部规划司某某处长1日表示，十二五工业发展规划正加紧起草中。</p>
+</li>
+
+</ul>
+</div>
+   <div class="module_footer"><span class="l"></span><span class="r"></span></div>
+ </div>
+</div>
+
+ </div>
+ <div class="clear main_box_inner_bottom"></div>
+</div><div class="bottom_box"><div class="bottom_info">
+	<a href="index.aspx">网站首页</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a href="#">关于我们</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a href="#">新闻资讯</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a href="#">产品中心</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a href="#">成功案例</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a href="#">人才招聘</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a href="#">在线留言</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a href="#">联系我们</a><b><br />
+联系人：</b>黄小姐&nbsp; <b>手机：</b>135123456789&nbsp; <b>电话：</b>0760-22517081&nbsp;&nbsp;<b>传真：</b>0760-86744123&nbsp;&nbsp;<b>邮件：</b>service#pageadmin.net &nbsp;&nbsp;<br />
+PageAdmin<a href="http://www.pageadmin.net/" target="_blank">企业网站管理系统</a>版权所有&nbsp;2008-2015 粤ICP备07048220号&nbsp;
+</div>
+</div><div align='center' style='padding:5px'>Powered by <a href='http://www.pageadmin.net' target='_blank'><strong>PageAdmin CMS</strong></a></div></div></body><script type="text/javascript">TongJi(1)</script></html>
---
+++ index1.php
@@ -0,0 +1,116 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+    <title>美丽长寿湖</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta http-equiv="Content-Type" content="text/html;charset=utf-8"/>
+    <link href="public/css/style.css" type="text/css" rel="stylesheet"/>
+    <link href="public/css/dropmenu.css" rel="stylesheet" type="text/css"/>
+    <script src="public/js/jquery.min.js" type="text/javascript"></script>
+    <script src="public/js/comm.js" type="text/javascript"></script>
+    <script src="public/js/zh-cn/lang.js" type="text/javascript"></script>
+    <script type="text/javascript">var submenu_style = 1;</script>
+    <script src="public/js/script.js" type="text/javascript"></script>
+    <script src="public/js/function.js" type="text/javascript"></script>
+</head>
+<body>
+<!-------------------------顶部开始--------------------------->
+<div class="page_style lanmupage page_l61">
+    <div class="top_box">
+        <div class="top_box_inner">
+            <div class="logo"></div>
+            <div class="topcontent">
+                <span class="top-l">欢迎光临长寿湖旅游网站!</span>
+                <span class="top-r">
+                    <script src="public/js/TtoS.js"></script>
+                    <a href="javascript:void(0)">登陆</a>
+                    <a href="javascript:void(0)">注册</a>
+                </span>
+            </div>
+            <div class="clear"></div>
+<!------------------------顶部结束----------------------------->
+<!------------------------导航栏开始----------------------------->
+            <div class="menu_box" id="Menu">
+                <ul>
+                    <li class="menu_style_homepage menu_current" id="MenuItem61" name="MenuItem"><a href="" class="menu">网站首页</a></li>
+                    <li class="menu_style" id="MenuItem62" name="MenuItem"><a href="jqjs/" class="menu">景区介绍</a></li>
+                    <li class="menu_style" id="MenuItem63" name="MenuItem"><a href="jqzx/" class="menu">景区资讯</a></li>
+                    <li class="menu_style" id="MenuItem64" name="MenuItem"><a href="javascript:void(0)" class="menu">吃·住·玩·购</a>
+                        <ul>
+                            <li><a href="czwg/eat">"吃"在长寿湖</a></li>
+                            <li><a href="czwg/live">"住"在长寿湖</a></li>
+                            <li><a href="czwg/play">"玩"在长寿湖</a></li>
+                            <li><a href="czwg/buy">"购"在长寿湖</a></li>
+                        </ul>
+                    </li>
+                    <li class="menu_style" id="MenuItem65" name="MenuItem"><a href="zxly/" class="menu">在线留言</a></li>
+                    <li class="menu_style" id="MenuItem66" name="MenuItem"><a href="lxwm/" class="menu">联系我们</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <!--显示二级菜单-->
+    <script type="text/javascript">var IsPageHome = "1";var Lanmu_Id = "61";var Sublanmu_Id = "0";ShowSubMenu(Lanmu_Id);</script>
+<!------------------------导航栏结束----------------------------->
+<!------------------------轮播图开始----------------------------->
+    <div class="banner" style="height:350px;width:1000px;background: grey;margin:10px auto;">
+        轮播图
+    </div>
+</div>
+<!------------------------轮播图结束----------------------------->
+
+<!------------------------=====中间内容版块开始=====----------------------------->
+<!------------------------景区介绍开始----------------------------->
+<div id="jq_box">
+    <div id="jqjs_box">
+
+    </div>
+<!------------------------景区介绍结束----------------------------->
+<!------------------------景区资讯开始----------------------------->
+    <div id="jqzx_box">
+
+    </div>
+</div>
+<!------------------------景区资讯结束----------------------------->
+<!------------------------热门推荐开始----------------------------->
+<div id="rmtj_box">
+    <div id="jqjs_box1">
+
+    </div>
+    <div id="jqjs_box2">
+
+    </div>
+    <div id="jqjs_box3">
+
+    </div>
+    <div id="jqjs_box4">
+
+    </div>
+    <div id="jqjs_box5">
+
+    </div>
+    <div id="jqjs_box6">
+
+    </div>
+</div>
+<!------------------------热门推荐结束----------------------------->
+<!------------------------=====中间内容板块结束====----------------------------->
+
+<!------------------------底部开始----------------------------->
+    <div class="bottom_box">
+        <div class="bottom_info">
+            <a href="index.aspx">网站首页</a>&nbsp;&nbsp;|&nbsp;&nbsp;
+            <a href="#">景区介绍</a>&nbsp;&nbsp;|&nbsp;&nbsp;
+            <a href="#">景区资讯</a>&nbsp;&nbsp;|&nbsp;&nbsp;
+            <a href="#">吃住玩购</a>&nbsp;&nbsp;|&nbsp;&nbsp;
+            <a href="#">在线留言</a>&nbsp;&nbsp;|&nbsp;&nbsp;
+            <a href="#">联系我们</a>
+            <b><br/>联系人：</b>BinGo&nbsp;
+            <b>手机：</b>135123456789&nbsp;
+            <b>邮箱：</b>619280492@qq.com<br/>
+            Copyright ©1996-2016 CSH Corporation, All Rights Reserved
+        </div>
+    </div>
+<!------------------------底部结束----------------------------->
+</body>
+</html>
