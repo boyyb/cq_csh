1146de610680ec1acca0d4dee1271ffc9863635b
yangbin <619280492@qq.com>
2016-08-04 02:17:20 +0800

    a

--- public/js/comm.js
+++ public/js/comm.js
@@ -31,9 +31,9 @@
             mode:'vertical', //默认的是水平
             //displaySlideQty:1,//显示li的个数
             //moveSlideQty: 1,//移动li的个数
-            captions: true,//自动控制
+            //captions: true,//自动控制
             auto: true,
-            controls: true//隐藏左右按钮
+            controls: false//隐藏左右按钮
     });
 });
 
