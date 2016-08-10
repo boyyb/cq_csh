2afa282dc7f02ff9a083b22a8bc01b3bab600eda
yangbin <619280492@qq.com>
2016-08-10 23:15:35 +0800

    admin

---
+++ admin/Application/Runtime/Cache/Home/51f56f8f375e1b2493439729eff22748.php
@@ -0,0 +1,10 @@
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>welcome</title>
+</head>
+<body>
+<p align="center"><h3>欢迎管理员进入后台管理系统！</h3></p>
+</body>
+</html>
