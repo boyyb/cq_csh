7dfccb057c3441f2cf25b9988e72560e7005140e
yangbin <619280492@qq.com>
2016-08-16 17:03:41 +0800

    0816p

--- admin/Application/Home/Controller/AdminController.class.php
+++ admin/Application/Home/Controller/AdminController.class.php
@@ -2,8 +2,45 @@
 namespace Home\Controller;
 use Think\Controller;
 class AdminController extends Controller {
-    public function index(){
+    public function add($id=0){
+        $data = array();
+        if($id){
+            $data = D('admin')->where("id=$id")->select();
+            dump($data);
+        }
+
+        $this -> assign("data",$data);
         $this -> display();
+    }
+
+    public function checkUsername(){
+        die("0");
+        $username =$_REQUEST['username'];
+        $data = D('admin')->where("username=$username")->select();
+        if($data){
+            echo "1";
+        }else{
+            echo "0";
+        }
+    }
+
+    public function save(){
+        $_REQUEST['reg_time']=time();
+        $lastid = D('admin')->add($_REQUEST);
+        if($lastid){
+            $this->success("添加成功","add",5);
+        }else{
+            $this->error("添加失败","add",5);
+        }
+    }
+
+    public function alist(){
+        $data = array();
+       // $alldata = D('admin')->select();
+       // if($alldata){$data = $alldata;}
+        $this->assign("data",$data);
+        $this->display();
+
     }
 
 }
--- admin/Application/Runtime/Cache/Home/5d134350630873c8925e06a839a4ff8e.php
+++ admin/Application/Runtime/Cache/Home/5d134350630873c8925e06a839a4ff8e.php
@@ -4,15 +4,21 @@
     <meta charset="UTF-8">
     <title>administrators</title>
     <link rel="stylesheet" href="/1_csh/admin/Public/Index_style/css/index.css" type="text/css" media="screen" />
+    <script src="/1_csh/admin/Public/Js/jquery-2.2.4.min.js"></script>
     <style>
         body{margin: 6px 0 0 10px;}
+
     </style>
+    <script>
+
+    </script>
 </head>
 <body>
 <div class="route_bg">
-    <a href="#">主页</a><i class="glyph-icon icon-chevron-right"></i>
-    <a href="#">管理员</a>
+    <a href="javascript:void(0)">管理员信息</a><i class="glyph-icon icon-chevron-right"></i>
+    <a href="javascript:void(0)">管理员列表</a>
 </div>
-<p>fdfdfdfdfsfsdff</p>
+
+
 </body>
 </html>
