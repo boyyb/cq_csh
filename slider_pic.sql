29fefc1fd1fa089cfb9d07f2ecf1bb4969525bd4
yangbin <619280492@qq.com>
2016-08-25 15:14:26 +0800

    0825-company

---
+++ slider_pic.sql
@@ -0,0 +1,14 @@
+/*
+SQLyog Enterprise v12.08 (32 bit)
+MySQL - 5.6.17 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+create table `slider_pic` (
+	`id` int (10),
+	`pic_name` varchar (60),
+	`jump_url` varchar (150),
+	`order` smallint (6),
+	`prompt` varchar (150)
+); 
